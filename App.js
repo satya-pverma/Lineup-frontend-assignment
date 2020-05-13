cacc07abef535041b053f74c1c1b9e247a7cf93a
satya-pverma <61929880+satya-pverma@users.noreply.github.com>
2020-05-13 10:39:09 +0530

    Create App.js
    This is App.js component

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Bodypart from './component/Bodypart';
+
+
+import Headernav from './component/Headernav';
+function App() {
+ 
+  return (
+    <div>
+            <Headernav />
+            
+            <Bodypart />
+            
+    </div>
+    
+  );
+}
+
+
+export default App;
